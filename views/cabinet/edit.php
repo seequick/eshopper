0d01626f7ba2a7d8cadc91b16638b146b6675ce6
h <howff@ukr.net>
2018-04-07 22:41:40 +0300

    L5 ready UPD

---
+++ views/cabinet/edit.php
@@ -0,0 +1,31 @@
+<?php
+require_once ROOT.'/views/layouts/header.php';?>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 col-sm-offset-4 padding-right">
+
+                <?php if ($result): ?>
+                    <p>Данные изменены!</p>
+                <?php else: ?>
+                    <?php if (isset($errors) && is_array($errors)): ?>
+                        <ul>
+                            <?php foreach ($errors as $error): ?>
+                                <li> - <?php echo $error; ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    <?php endif; ?>
+                    <div class="signup-form"><!--tipo edit form-->
+                        <h2>Редактирование данных</h2>
+                        <form action="" method="post">
+                            <input type="text" name="name" placeholder="Имя" value="<?php echo $name; ?>"/>
+                            <input type="password" name="password" placeholder="Пароль" value="<?php echo $password; ?>"/>
+                            <input type="submit" name="submit" class="btn btn-default" value="Сохранить"/>
+                        </form>
+                    </div><!--/edit form-->
+                <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php require_once ROOT.'/views/layouts/footer.php';?>
